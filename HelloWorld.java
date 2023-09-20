a1d40928197e13c5209a221757d87e623da5c4de
Elizabeth Barefoot <elizabeth.barefoot@mrjaffesclass.com>
2023-09-20 11:36:41 -0700

    commit message

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,19 +8,26 @@
 public class HelloWorld
 {
     
-    int a = 5;
-    int b = 2;
-    int c = 1;
-    int d = -8;
-    
     public void run() {
         int i = 0;
-        while (i > 15) {
-            System.out.println("i is" + i + " but is less than 15");
+        while (i < 15) {
+            System.out.println("i is " + i + " and is less than 15");
             i++;
         }
+        int k = 10;
+        while (k > 5) {
+            System.out.println("k is " + k + " and is greater than 5");
+            k--;
+        }
+        int a = 0;
+        int b = 0;
+        while (a < 10 && b <= 20) {
+            System.out.println("b is " + b + " and is less than or equal to 20");
+            System.out.println("a is " + a + " and is less than 10");
+            b++;
+            a++;
+        }
     }
-
     
     public static void main(String[] args)
     {        
